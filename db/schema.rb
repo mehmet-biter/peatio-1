195b2fc12b037b92c26e2c1036ef33305e16c7d1
mnaichuk <mnaichuk@heliostech.fr>
2020-08-25 17:15:43 +0300

    Add WithdrawLimit draft

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_143442) do
+ActiveRecord::Schema.define(version: 2020_08_25_105929) do
 
   create_table "accounts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "member_id", null: false
@@ -400,6 +400,20 @@ ActiveRecord::Schema.define(version: 2020_08_06_143442) do
     t.index ["kind", "currency_id", "status"], name: "index_wallets_on_kind_and_currency_id_and_status"
     t.index ["kind"], name: "index_wallets_on_kind"
     t.index ["status"], name: "index_wallets_on_status"
+  end
+
+  create_table "withdraw_limits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "currency_id", limit: 20, default: "any", null: false
+    t.string "group", limit: 32, default: "any", null: false
+    t.string "kyc_level", limit: 32, default: "any", null: false
+    t.decimal "l24hour", precision: 32, scale: 16, default: "0.0", null: false
+    t.decimal "l1month", precision: 32, scale: 16, default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["currency_id", "group", "kyc_level"], name: "index_withdraw_limits_on_currency_id_and_group_and_kyc_level", unique: true
+    t.index ["currency_id"], name: "index_withdraw_limits_on_currency_id"
+    t.index ["group"], name: "index_withdraw_limits_on_group"
+    t.index ["kyc_level"], name: "index_withdraw_limits_on_kyc_level"
   end
 
   create_table "withdraws", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
